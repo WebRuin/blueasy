070a31a1d7b8380895c8d81f6839b826652aaef3
Web <web@testpress.dev>
2018-03-17 17:22:00 -0700

    top and services

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,32 @@
 const gulp = require('gulp'),
-      sass = require('gulp-sass');
+      sass = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      imageop = require('gulp-image-optimization');
 
 gulp.task('styles', function() {
     gulp.src('src/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./build/css/'))
+        .pipe(gulp.dest('./build'))
+});
+
+gulp.task('autoprefixer', () =>
+    gulp.src('build/styles/main.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('build/styles'))
+);
+ 
+gulp.task('imagemin', function(cb) {
+    gulp.src(['src/**/*.png','src/**/*.jpg','src/**/*.gif','src/**/*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('build')).on('end', cb).on('error', cb);
 });
 
 //Watch task
 gulp.task('default', function() {
-    gulp.watch('src/**/*.scss',['styles']);
+    gulp.watch('src/**/*.scss',['styles', 'autoprefixer', 'imagemin']);
 });
